d3bd49bd8c23d88e7b6b03b4fcca4769bb28c346
Ronnie <ronnie_buck@hotmail.com>
2021-12-09 23:28:04 -0300

    Correção na remoção de um tarefa

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -11,8 +11,8 @@
       <svg xmlns="http://www.w3.org/2000/svg" class="icon pointer-events-none" viewBox="0 0 20 20" fill="currentColor">
         <path fill-rule="evenodd" d="M3 4a1 1 0 011-1h4a1 1 0 010 2H6.414l2.293 2.293a1 1 0 01-1.414 1.414L5 6.414V8a1 1 0 01-2 0V4zm9 1a1 1 0 110-2h4a1 1 0 011 1v4a1 1 0 11-2 0V6.414l-2.293 2.293a1 1 0 11-1.414-1.414L13.586 5H12zm-9 7a1 1 0 112 0v1.586l2.293-2.293a1 1 0 011.414 1.414L6.414 15H8a1 1 0 110 2H4a1 1 0 01-1-1v-4zm13-1a1 1 0 011 1v4a1 1 0 01-1 1h-4a1 1 0 110-2h1.586l-2.293-2.293a1 1 0 011.414-1.414L15 13.586V12a1 1 0 011-1z" clip-rule="evenodd" />
       </svg>
-      <h2 class="content pointer-events-none">{{ todo.description }}</h2>
-      <span class="dismiss pointer-events-none" :style="{backgroundColor: close}" @click="remove">X</span>
+      <h2 class="content">{{ todo.description }}</h2>
+      <span class="dismiss" :style="{backgroundColor: close}" @click="remove">X</span>
   </div>
 </template>
 
@@ -48,6 +48,7 @@ export default {
       bus.done(this.todo)
     },
     remove(){
+      console.log('removendo');
       bus.remove(this.index)
     },
     startDrag(event, todo) {
@@ -146,13 +147,10 @@ export default {
   text-decoration: line-through;
 }
 
-.pointer-events-none {
-  pointer-events: none;
-}
-
 .content {
   flex: 9;
   text-align: center;
+  pointer-events: none;
 }
 
 .dismiss {
